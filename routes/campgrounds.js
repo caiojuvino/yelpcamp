eebb58ec58562bcaca800eb527caaa3e8bde222c
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-04-26 20:51:36 -0300

    Uploading To Cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer = require('multer');
+const { storage } = require('../cloudinary');
 const upload = multer({ storage });
 
 const Campground = require('../models/campground');
