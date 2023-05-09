a87938863e6d81ac9786882167ee1ddc91223085
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-05-09 18:08:09 -0300

    Fixing Our Seeds Bug

--- seeds/index.js
+++ seeds/index.js
@@ -28,6 +28,10 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dsjoppwwl/image/upload/v1682947790/YelpCamp/g7ciuxbi3m8oe8000awa.jpg',
